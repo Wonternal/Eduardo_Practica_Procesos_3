8638d7a2e0626e546a8c5be06d1603a6aacd7f49
Eduardo Camacho Díaz <fgada200@gmail.com>
2022-10-19 13:51:38 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+         * 3- Desarrolla una tercera aplicación en Java que reciba como entrada una ruta a un fichero del
+         * sistema. Tu programa debe abrir el fichero, si existe, utilizando el comando cat y enviar la salida a la
+         * entrada del comando wc, que se utiliza para contar las líneas de un fichero. (4 puntos)
+         */
+
+        // Página con información relacionada al ejercicio.  (Fuentes)
+        // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/ProcessBuilder.html
+        // https://stackoverflow.com/questions/53189986/open-git-bash-using-processbuilder-and-execute-command-in-it
+        // https://stackoverflow.com/questions/3776195/using-java-processbuilder-to-execute-a-piped-command
+
+
+    }
+}
